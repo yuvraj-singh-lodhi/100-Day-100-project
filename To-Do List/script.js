63e0dce4201940f71fc56c160b85ebd54f6b26b6
yuvraj-singh-lodhi <it2005@global.org.in>
2023-11-14 16:02:58 +0530

    day 42 weather app

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -4,61 +4,47 @@ var ul = document.querySelector("ul");
 var item = document.getElementsByTagName("li");
 
 function inputLength(){
-	return input.value.length;
+    return input.value.length;
 } 
 
 function listLength(){
-	return item.length;
+    return item.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li"); // creates an element "li"
-	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
-	ul.appendChild(li); //adds li to ul
-	input.value = ""; //Reset text input field
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
 
+    function crossOut() {
+        li.classList.toggle("done");
+    }
 
-	//START STRIKETHROUGH
-	// because it's in the function, it only adds it for new items
-	function crossOut() {
-		li.classList.toggle("done");
-	}
+    li.addEventListener("click", crossOut);
 
-	li.addEventListener("click",crossOut);
-	//END STRIKETHROUGH
-
-
-	// START ADD DELETE BUTTON
-	var dBtn = document.createElement("button");
-	dBtn.appendChild(document.createTextNode("X"));
-	li.appendChild(dBtn);
-	dBtn.addEventListener("click", deleteListItem);
-	// END ADD DELETE BUTTON
-
-
-	//ADD CLASS DELETE (DISPLAY: NONE)
-	function deleteListItem(){
-		li.classList.add("delete")
-	}
-	//END ADD CLASS DELETE
+    var dBtn = document.createElement("button");
+    dBtn.innerHTML = '<i class="fas fa-times"></i>';
+    li.appendChild(dBtn);
+    dBtn.addEventListener("click", deleteListItem);
 }
 
+function deleteListItem() {
+    var li = this.parentNode;
+    li.classList.add("delete");
+}
 
 function addListAfterClick(){
-	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
-		createListElement();
-	}
+    if (inputLength() > 0) {
+        createListElement();
+    }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.which ===13) { //this now looks to see if you hit "enter"/"return"
-		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
-		createListElement();
-	} 
+    if (inputLength() > 0 && event.which === 13) {
+        createListElement();
+    } 
 }
 
-
-enterButton.addEventListener("click",addListAfterClick);
-
+enterButton.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
-
---
+++ Weather App/script.js
@@ -0,0 +1,87 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const input = document.querySelector('.search-box input');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+const welcomeMessage = document.querySelector('.welcome-message');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const welcomeMessage = document.querySelector('.welcome-message');
+    welcomeMessage.classList.add('active');
+  });
+  
+
+const fetchWeatherData = () => {
+    const APIKey = '115a2babd131690c595764d3936dbfa1';
+    const city = input.value;
+
+   
+    if (city === '' && container.style.height === '555px') {
+        welcomeMessage.classList.add('active');
+        return;
+    } else {
+        welcomeMessage.classList.remove('active');
+    }
+
+    if (city === '') return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+            if (json.cod == '404') {
+                container.style.height = '404px';
+                weatherBox.classList.remove('active');
+                weatherDetails.classList.remove('active');
+                error404.classList.add('active');
+                return;
+            }
+            container.style.height = '555px';
+            weatherBox.classList.add('active');
+            weatherDetails.classList.add('active');
+            error404.classList.remove('active');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+                case 'Mist':
+                    image.src = 'images/mist.png';
+                    break;
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+                default:
+                    image.src = 'images/cloud.png';
+            }
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+        });
+};
+
+
+search.addEventListener('click', fetchWeatherData);
+
+
+input.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+        fetchWeatherData();
+    }
+});
