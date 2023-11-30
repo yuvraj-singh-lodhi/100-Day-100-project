de6d67ee0cd9ae737c1969adb257807452dc485c
yuvraj-singh-lodhi <it2005@global.org.in>
2023-11-30 23:30:52 +0530

    day 53 to 56 Quiz app

---
+++ REACT'S PROJECTS/Quiz-App/src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "What does HTML stand for?",
+        option1: "HyperText Markup Language",
+        option2: "HighlyText Markup Language",
+        option3: "HyperText Management Language",
+        option4: "Hyperlink and Text Markup Language",
+        ans: 1
+    },
+    {
+        question: "Which tag is used to create an unordered list in HTML?",
+        option1: "<ol>",
+        option2: "<ul>",
+        option3: "<li>",
+        option4: "<dl>",
+        ans: 2
+    },
+    {
+        question: "In CSS, what property is used to change the text color of an element?",
+        option1: "color",
+        option2: "text-color",
+        option3: "font-color",
+        option4: "text-style",
+        ans: 1
+    },
+    {
+        question: "What is the purpose of the JavaScript 'typeof' operator?",
+        option1: "To check if a variable is defined",
+        option2: "To determine the type of a variable",
+        option3: "To compare two variables",
+        option4: "To create a new variable",
+        ans: 2
+    },
+    {
+        question: "Which of the following is NOT a JavaScript framework or library?",
+        option1: "React",
+        option2: "Angular",
+        option3: "JavaFX",
+        option4: "Vue.js",
+        ans: 3
+    }
+];
