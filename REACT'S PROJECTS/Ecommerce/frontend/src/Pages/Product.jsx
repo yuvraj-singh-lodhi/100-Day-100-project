a06650dce704f1c8a2efa896edcd9c1ed1d2a4a9
yuvraj-singh-lodhi <it2005@global.org.in>
2024-05-10 21:41:08 +0530

    added pages in image slider

---
+++ REACT'S PROJECTS/Ecommerce/frontend/src/Pages/Product.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../Context/shopcontext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+
+const Product = () => {
+  const{all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId));
+  return (
+    <div>
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
+    </div>
+  )
+}
+
+export default Product
